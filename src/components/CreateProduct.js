26485357f764a6ce88aae8296cb0d003b2e1eb8b
John <johnpua@outlook.com>
2023-07-14 21:23:13 -0700

    Fullstack MERN productManager

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const CreateProduct = () => {
-  const [title, setTitle] = useState('');
-  const [price, setPrice] = useState('');
-  const [description, setDescription] = useState('');
+const ProductForm = () => {
+const [title, setTitle] = useState('');
+const [price, setPrice] = useState('');
+const [description, setDescription] = useState('');
 
-  const handleSubmit = async (e) => {
+const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('/api/products', {
+    const response =await axios.post('/api/products', {
         title,
         price,
         description,
-      });
+    });
 
-      if (response.status === 201) {
+    if (response.status === 201) {
         // Product created successfully
         const product = response.data;
         console.log(product);
@@ -24,55 +24,55 @@ const CreateProduct = () => {
         setTitle('');
         setPrice('');
         setDescription('');
-      } else {
+    } else {
         // Handle error case
         console.error('Product creation failed');
-      }
-    } catch (error) {
-      console.error('An error occurred', error);
     }
-  };
+    } catch (error) {
+    console.error('An error occurred', error);
+    }
+};
 
-  return (
+return (
     <div>
-      <h1>Product Manager</h1>
-      <form onSubmit={handleSubmit}>
+    <h1>Product Manager</h1>
+    <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="title">Title:</label>
-          <input
+        <label htmlFor="title">Title:</label>
+        <input
             type="text"
             id="title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             placeholder="Title"
             required
-          />
+        />
         </div>
         <div>
-          <label htmlFor="price">Price:</label>
-          <input
+        <label htmlFor="price">Price:</label>
+        <input
             type="number"
             id="price"
             value={price}
             onChange={(e) => setPrice(e.target.value)}
             placeholder="Price"
             required
-          />
+        />
         </div>
         <div>
-          <label htmlFor="description">Description:</label>
-          <textarea
+        <label htmlFor="description">Description:</label>
+        <textarea
             id="description"
             value={description}
-            onChange={(e) => setDescription(e.target.value)}
+        onChange={(e) => setDescription(e.target.value)}
             placeholder="Description"
             required
-          ></textarea>
+        ></textarea>
         </div>
         <button type="submit">Create Product</button>
-      </form>
+    </form>
     </div>
-  );
+);
 };
 
-export default CreateProduct;
+export default ProductForm;
